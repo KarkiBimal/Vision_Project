eecf3ba5a1cc4d69467568403abebf61091cd5f9
KarkiBimal <karkibimal0@gmail.com>
2020-07-10 13:14:29 -0500

    changes on transcript form

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField
 from wtforms.validators import DataRequired, Length, Email , EqualTo, Length, ValidationError
 from application.models import User
 
@@ -48,7 +48,7 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That email is taken already')
 
 class Transcriptform(FlaskForm):
-    transcript1=StringField('Transcript1',validators=[Length(min=2, max=20)])
-    transcript2=StringField('Transcript2',validators=[Length(min=2, max=20)])
-    transcript3=StringField('Transcript3',validators=[Length(min=2, max=20)])
+    transcript1=TextField('Transcript1',validators=[Length(min=2, max=2000)])
+    transcript2=TextField('Transcript2',validators=[Length(min=2, max=2000)])
+    transcript3=TextField('Transcript3',validators=[Length(min=2, max=2000)])
     submit=SubmitField('Submitted')
